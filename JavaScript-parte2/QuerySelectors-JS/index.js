97a67b21cce32019150e05fe4ab4b161aa7bf012
Qele Andrade <quele.andrade43@gmail.com>
2021-12-05 09:41:46 -0300

    Pegando os elementos por Query selector

--- JavaScript-parte2/QuerySelectors-JS/index.js
+++ JavaScript-parte2/QuerySelectors-JS/index.js
@@ -7,3 +7,9 @@ console.log(document.getElementsByTagName("h1"));
 
 //pega os elementos por Class name
 console.log(document.getElementsByClassName("classe-primeiroh1"));
+
+//Pegar os elementos por Query selector
+console.log(document.querySelector(".classe-primeiroh1").innerHTML); //innerHTML mostra o valor
+console.log(document.querySelector("#primeiroh2").innerHTML); //pegando o valor por ID
+console.log(document.querySelector("#minha-lista").childNodes); //pega os elementos filhos dessa lista
+console.log(document.querySelector("#minha-lista #banana")); //acessando um filho especifico da lista
