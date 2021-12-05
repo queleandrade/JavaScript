010e132be575c4e719b63b5159a2d64199e82feb
Qele Andrade <quele.andrade43@gmail.com>
2021-12-05 09:09:55 -0300

    Adicionando elementos ID e Tag name

---
+++ JavaScript-parte2/QuerySelectors-JS/index.js
@@ -0,0 +1,6 @@
+//pega os elementos por ID
+console.log(document.getElementById("primeiroh1").innerText); //acessa o documento da DOM
+console.log(document.getElementById("primeiroh2").innerText); 
+
+//pega os elementos por Tag name
+console.log(document.getElementsByTagName("h1"); 
